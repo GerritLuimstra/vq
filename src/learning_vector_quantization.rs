293fe61dbf2693a7e003d2683d5471bef0d25609
Gerrit Sijberen Luimstra <gerrit.luimstra@protonmail.com>
2021-04-21 12:28:37 +0200

    Implemented the training loop for the LVQ model. Updated the documentation and changed the API of the LVQ model a little so that multiple prototypes can be given per class in a straightforward way.

--- src/learning_vector_quantization.rs
+++ src/learning_vector_quantization.rs
@@ -3,8 +3,9 @@ use super::LearningVectorQuantization;
 use super::helpers::euclidean_distance;
 
 use ndarray::Array1;
-use rand::prelude::thread_rng;
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
+use rand::prelude::thread_rng;
 
 impl LearningVectorQuantization {
 
@@ -12,12 +13,14 @@ impl LearningVectorQuantization {
     /// 
     /// # Arguments
     /// 
-    /// * `num_prototypes` The amount of prototypes to use per class (a vector, so this can be variable)
+    /// * `num_prototypes` The amount of prototypes to use per class (a hashmap, that maps the class name to the number of prototypes to use)
+    ///                    This hashmap should be provided as a reference and the algorithm will panic if there are classes 
+    ///                    in the data not present in this hashmap.
     /// * `learning_rate`  The learning rate for the update step of the prototypes
     /// * `max_epochs`     The amount of epochs to run
     /// * `prototypes`     A vector of the prototypes (initially empty)
     /// 
-    pub fn new ( num_prototypes: Vec<usize>, 
+    pub fn new ( num_prototypes: HashMap<String, usize>, 
                  learning_rate: f64,
                  max_epochs: u32, 
                  seed: Option<u32> ) -> LearningVectorQuantization {
@@ -65,8 +68,81 @@ impl LearningVectorQuantization {
     /// 
     /// * `data` The data to adapt the prototypes on
     /// 
-    pub fn fit (&mut self, data : &Vec<Array1<f64>>) {
-        assert!(false, "NOT YET IMPLEMENTED.");
+    pub fn fit (&mut self, data : &Vec<Array1<f64>>, labels : &Vec<String>) {
+
+        // Compute the total amount of prototypes given
+        let mut total_prototypes = 0;
+        for num_prototypes in self.num_prototypes.values() {
+            total_prototypes += num_prototypes;
+        }
+
+        // Assert that there is enough data
+        assert!(data.len() > total_prototypes,
+                "There are more prototypes than data samples. Consider lowering the amount of prototypes.");
+
+        // Assert that the model has not been fit yet
+        assert!(self.prototypes.len() == 0, "This model has already been fit.");
+
+        // Setup the prototypes by grabbing the respective amount of vectors from the data
+        // based on the amount of prototypes specified for that class
+        for (index, (class_name, num_prototypes)) in self.num_prototypes.iter().enumerate() {
+
+            // Obtain all the data samples with the class 'class_name'
+            let mut data_samples_by_class = vec![];
+            for (index, sample_label) in labels.iter().enumerate() {
+                if sample_label == class_name {
+                    data_samples_by_class.push(data[index].clone());
+                }
+            }
+
+            // Obtain a random prototype from the data samples by class and clone/own it
+            let selected_prototype = data_samples_by_class.choose(&mut rand::thread_rng()).unwrap();
+            let selected_prototype = selected_prototype.clone();
+            let selected_prototype = Prototype::new(selected_prototype, class_name.clone());
+
+            // Add the newly created prototypes to the prototype list
+            self.prototypes.push(selected_prototype);
+        }
+
+        for _epoch in 1 .. self.max_epochs + 1 {
+
+            // Shuffle the data to prevent artifacts during training
+            // We should be careful to shuffle the labels and data in the same matter
+            let mut shuffled_indices : Vec<usize> = (0 .. data.len()).collect();
+            shuffled_indices.shuffle(&mut thread_rng());
+
+            // Create shuffled (and cloned) data based on the shuffled indices
+            let mut shuffled_data   = vec![];
+            let mut shuffled_labels = vec![];
+            for index in shuffled_indices {
+                shuffled_data.push(data[index].clone());
+                shuffled_labels.push(labels[index].clone());
+            }
+
+            // Iterate over the shuffled data and update the closest prototype
+            for (index, data_sample) in shuffled_data.iter().enumerate() {
+
+                // Find the closest prototype to the data point
+                let closest_prototype_index = self.find_closest_prototype(&data_sample);
+                let closest_prototype       = self.prototypes.get(closest_prototype_index).unwrap();
+
+                // Compute the difference vector (the 'error')
+                let difference = (data_sample - closest_prototype.vector.clone());
+
+                // Update the current prototype by either moving it closer to the data sample
+                // if the classes of the data sample and the closest prototype match and move it
+                // further away otherwise.
+                let new_prototype;
+                if shuffled_labels[index] == closest_prototype.name {
+                    new_prototype = closest_prototype.vector.clone() + self.learning_rate * difference;
+                } else {
+                    new_prototype = closest_prototype.vector.clone() - self.learning_rate * difference;
+                }
+
+                // Replace the old prototype
+                self.prototypes[closest_prototype_index].vector = new_prototype;
+            }
+        }
     }
 
     /// Assign cluster labels (i.e. predict) to the data given data
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use ndarray::Array1;
+use std::collections::HashMap;
 
 mod helpers;
 mod prototype;
@@ -19,7 +20,8 @@ pub struct Prototype {
 
 /// The Vector Quantization model
 /// 
-/// This struct and its methods allow the modeling of probability density functions of a given data set by the distribution of prototype vectors. 
+/// This struct and its methods allow the modeling of probability density functions of a given data set by the
+/// distribution of prototype vectors using stochastic gradient descent.
 /// It works by dividing a large set of points (vectors) into groups having approximately the same number of points closest to them. 
 /// Each group is represented by its centroid point, in this case a prototype vector.
 /// 
@@ -44,7 +46,7 @@ pub struct VectorQuantization {
 
 /// The Learning Vector Quantization model
 /// 
-/// This struct and its methods provide an implementation of the LVQ model.
+/// This struct and its methods provide an implementation of the LVQ model using stochastic gradient descent.
 /// 
 /// An LVQ system is represented by prototypes which are defined in the feature space of observed data. 
 /// In a Hebbian-like winner-take-all fashion the algorithm determines
@@ -58,14 +60,14 @@ pub struct VectorQuantization {
 /// This specific implementation allows for a variable number of prototypes per class.
 /// 
 /// # Properties
-/// * `num_prototypes` The amount of prototypes to use per class (a vector, so this can be variable)
+/// * `num_prototypes` The amount of prototypes to use per class (a hashmap, that maps the class name to the number of prototypes to use)
 /// * `learning_rate`  The learning rate for the update step of the prototypes
 /// * `max_epochs`     The amount of epochs to run
 /// * `prototypes`     A vector of the prototypes (initially empty)
 /// 
 #[derive(Debug)]
 pub struct LearningVectorQuantization {
-    num_prototypes : Vec<usize>,
+    num_prototypes : HashMap<String, usize>,
     learning_rate : f64,
     max_epochs : u32, 
     seed : Option<u32>, // TODO: Implement
